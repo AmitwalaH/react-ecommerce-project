7acaf793f266a7807745b4e402bfa76139a4b611
AmitwalaH <algoworks818@gmail.com>
2025-11-10 15:25:36 +0530

    feat: Use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.js
+++ ecommerce-project/src/pages/home/Product.test.js
@@ -1,14 +1,17 @@
-import { describe, expect, it, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { Product } from "./Product";
 import axios from "axios";
+import { Product } from "./Product";
 
 vi.mock("axios");
 
 describe("Product component", () => {
-  it("display the product details correctly", () => {
-    const product = {
+  let product;
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -24,8 +27,10 @@ describe("Product component", () => {
     //we should not connect the backend , always create the MOCK for best practice
     //will use vi.fn() -> creates a fake function that doesn't do anything
 
-    const loadCart = vi.fn();
+    loadCart = vi.fn();
+  });
 
+  it("displays the product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />);
 
     expect(
@@ -34,42 +39,20 @@ describe("Product component", () => {
 
     expect(screen.getByText("$10.90")).toBeInTheDocument();
 
-    expect(
-      screen
-        .getByTestId("produc-image")
-        .toHaveAttribute(
-          "src",
-          "images/products/athletic-cotton-socks-6-pairs.jpg"
-        )
+    expect(screen.getByTestId("product-image")).toHaveAttribute(
+      "src",
+      "images/products/athletic-cotton-socks-6-pairs.jpg"
     );
 
-    expect(
-      screen
-        .getByTestId("product-rating-stars")
-        .toHaveAttribute("src", "images/ratings/rating-45.png")
+    expect(screen.getByTestId("product-rating-stars-image")).toHaveAttribute(
+      "src",
+      "images/ratings/rating-45.png"
     );
+
     expect(screen.getByText("87")).toBeInTheDocument();
   });
 
   it("adds a product to the cart", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-
-    //create the fake function so to avoid any problems to backend
-    //we should not connect the backend , always create the MOCK for best practice
-    //will use vi.fn() -> creates a fake function that doesn't do anything
-
-    const loadCart = vi.fn();
-
     render(<Product product={product} loadCart={loadCart} />);
 
     const user = userEvent.setup();
